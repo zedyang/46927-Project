154b721849f80b6f014df1056f2246b7998878f1
zedyang <zed.yang@outlook.com>
2018-04-10 13:04:14 -0400

    04/10/18

--- emp_risk_optimizer.py
+++ emp_risk_optimizer.py
@@ -1,6 +1,8 @@
 import abc
 import sys
 import time
+import json
+from collections import OrderedDict
 
 import numpy as np
 import pandas as pd
@@ -39,20 +41,22 @@ class EmpiricalRiskOptimizer(BaseEstimator, TransformerMixin):
         self.hyperparams = ['eta']
 
         # Variables and Inputs
-        self.all_params = None
+        self.n_params = None
+        self.all_params_dict, self.all_params = OrderedDict(), None
         self.X_tr, self.y_tr = None, None
         self.X_te, self.y_te = None, None
         self.feed_dict = None
-        self.emp_risk = None
+        self.emp_risk, self.losses = None, None
         self.train_op = None
         self.grad_emp_risk = None
+        self.grad_L = None
         if self.eval_hessian:
             self.hessian_emp_risk = None
 
     def __repr__(self):
         hyperparams_str = ','.join([
-            '{}={}'.format(p, getattr(self, p))
-            for p in self.hyperparams])
+            '{}={}'.format(p_, getattr(self, p_))
+            for p_ in self.hyperparams])
         __repr = self.model_name + '({})'.format(hyperparams_str)
         return __repr
 
@@ -63,10 +67,14 @@ class EmpiricalRiskOptimizer(BaseEstimator, TransformerMixin):
         self.y_tr = tf.placeholder(
             tf.float32, (self.n, 1), name='y_tr')
         self.all_params = self.get_all_params()
-        self.emp_risk = self.get_emp_risk()
+        self.losses, self.emp_risk = self.get_emp_risk()
         self.train_op = self.get_op()
         self.grad_emp_risk = tf.gradients(self.emp_risk, self.all_params)
-        self.hessian_emp_risk = tf.hessians(self.emp_risk, self.all_params)
+        self.hessian_emp_risk = tf.hessians(
+            self.emp_risk, self.all_params)
+        self.grad_L = [tf.placeholder(
+            tf.float32, shape=a.get_shape())
+            for a in self.all_params]
 
         # initialize all variables
         init = tf.global_variables_initializer()
@@ -98,6 +106,9 @@ class EmpiricalRiskOptimizer(BaseEstimator, TransformerMixin):
         train_op = optimizer.minimize(self.emp_risk)
         return train_op
 
+    def get_params_by_name(self, key):
+        return self.all_params
+
     def get_all_params(self):
         raise NotImplementedError
 
@@ -115,15 +126,19 @@ class EmpiricalRiskOptimizer(BaseEstimator, TransformerMixin):
                 [self.grad_emp_risk, self.hessian_emp_risk],
                 feed_dict=self.feed_dict
             )
-            grads = np.vstack([g for g in grad_loss_val])
-            dim = np.prod(all_params_val.shape)
-            hessian = np.vstack([
-                h for h in hessian_loss_val]).reshape(dim, dim)
+            params = {key: val for key, val in zip(
+                self.all_params_dict.keys(), all_params_val)}
+            grads = {key: val for key, val in zip(
+                self.all_params_dict.keys(), grad_loss_val)}
+            grads_stacked = np.vstack(grad_loss_val)
+            #dim = np.prod(all_params_val.shape)
+            hessian = None
             return {
-                'params': all_params_val,
+                'params': params,
                 'loss': loss_val,
                 'grads': grads,
-                'hessian': hessian
+                'grads_stacked': grads_stacked,
+                'hessian': hessian_loss_val
             }
         else:
             # only retrieve parameters
@@ -142,8 +157,38 @@ class LinearRegressionGD(EmpiricalRiskOptimizer):
         super(LinearRegressionGD, self).__init__(**kwargs)
 
     def get_all_params(self):
-        return tf.get_variable(
-            'beta', (self.p, 1), initializer=tf.zeros_initializer)
+        self.n_params = self.p
+        self.all_params_dict['beta'] = tf.get_variable(
+            'beta', (self.n_params,1),
+            initializer=tf.zeros_initializer)
+
+        return list(self.all_params_dict.values())
+
+    def get_emp_risk(self):
+        beta = self.all_params_dict['beta']
+        y_hat = tf.matmul(self.X_tr, beta, name='y_hat')
+        losses = tf.pow(self.y_tr - y_hat, 2, name='l2_loss')
+        emp_risk = tf.reduce_mean(losses, name='emp_risk')
+        #stacked_params =
+        return losses, emp_risk
+
+    def predict(self, X_test):
+        beta = self.get_eval(all_eval=False)
+        return X_test.dot(beta)
+
+
+class LinearRegressionGDSplitParams(EmpiricalRiskOptimizer):
+    """
+    Only for debug purposes.
+    """
+
+    def __init__(self, **kwargs):
+        super(LinearRegressionGDSplitParams, self).__init__(**kwargs)
+
+    def get_all_params(self):
+        return [tf.get_variable(
+            'beta', (self.p, 1),
+            initializer=tf.random_normal_initializer)]
 
     def get_emp_risk(self):
         beta = self.all_params
@@ -154,3 +199,17 @@ class LinearRegressionGD(EmpiricalRiskOptimizer):
     def predict(self, X_test):
         beta = self.get_eval(all_eval=False)
         return X_test.dot(beta)
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('data/leverage.csv')
+    X = df.values[:, 0:2]
+    n, p = X.shape
+    y = df.values[:, -1].reshape(n, 1)
+    model = LinearRegressionGD(
+        model_name='LinearRegression',
+        eta=0.001)
+    model.fit(X,y,n_iter=10000)
+    print(model.get_eval())
+    #print(json.dumps(model.get_eval(), sort_keys=True,
+    #                 indent=4, separators=(',', ': ')))
